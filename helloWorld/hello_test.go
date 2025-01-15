5d481f67cad406fbf9243bf0af7c804bb5824217
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-15 19:28:43 +1300

    Refactor Hello World Tests

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -7,16 +7,21 @@ func TestHello(t *testing.T) {
 		got := Hello("Sam")
 		want := "Hello, Sam"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+
+	
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
-	})
 }

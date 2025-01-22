f26c6523993648472202edf7e464ba2db497c071
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-22 17:53:23 +1300

    Add specific field definitions to tests for clarity

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -19,9 +19,9 @@ func TestArea(t *testing.T) {
 		shape Shape
 		want float64
 	} {
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		{shape: Rectangle{12, 6}, want: 72.0},
+		{shape: Circle{10}, want: 314.1592653589793},
+		{shape: Triangle{12, 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {

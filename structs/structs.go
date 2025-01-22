6f7f3ee6ad55f5c8255d7638e5af86c82fed3cc5
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-22 15:16:08 +1300

    Refactor functions using rectangle struct

--- structs/structs.go
+++ structs/structs.go
@@ -5,10 +5,10 @@ type Rectangle struct {
 	Height float64
 }
 
-func Perimeter(height, width float64) float64 {
-	return 2*height + 2*width
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
 }
 
-func Area(height, width float64) float64 {
-	return height * width
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Height * rectangle.Width
 }

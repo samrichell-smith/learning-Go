30e1305662da273c4520216d3f3e320900ab3e02
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-23 15:13:37 +1300

    Implement wallet methods

--- banking/banking.go
+++ banking/banking.go
@@ -1,8 +1,16 @@
 package banking
 
+import "fmt"
+
 type Wallet struct {
+	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("adress of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
 
+func (w Wallet) Balance() int {
+	return w.balance
 }

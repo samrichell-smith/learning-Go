8d050b76e93a2579cac8a4c6e158ac5f2482aca5
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-24 22:22:20 +1300

    Add intentional fail case for dictionary searching

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -1,11 +1,19 @@
 package dictionary
 
+import "errors"
+
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
 }
 
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }

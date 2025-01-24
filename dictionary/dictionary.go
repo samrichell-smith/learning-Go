186653ea447d5c3f94562b4488fabac3375101e2
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-24 22:23:25 +1300

    Refactor error handling

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -8,11 +8,14 @@ func Search(dictionary map[string]string, word string) string {
 
 type Dictionary map[string]string
 
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil

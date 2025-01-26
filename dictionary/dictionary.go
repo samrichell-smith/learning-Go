85a6a980e8d34e3e20b31b2a998bc459a7140a19
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-26 18:46:18 +1300

    Add edge case updating unit tests

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -3,8 +3,9 @@ package dictionary
 type Dictionary map[string]string
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("could not perform operations on word as it does not exist")
 )
 
 type DictionaryErr string
@@ -39,6 +40,7 @@ func (d Dictionary) Add(word string, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, newDefinition string) {
+func (d Dictionary) Update(word, newDefinition string) error {
 	d[word] = newDefinition
+	return nil
 }

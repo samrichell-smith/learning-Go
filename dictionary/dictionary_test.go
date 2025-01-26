6ce32f6b01abb59efb82f4c4684286b5cc0793eb
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-26 17:51:45 +1300

    Create Add Entry unit test

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -39,3 +39,16 @@ func assertError(t testing.TB, got, want error) {
 		t.Errorf("got %q want %q", got, want)
 	}
 }
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, want)
+}

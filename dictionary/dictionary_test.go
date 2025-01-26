5120221498bad3dabd1d1bb761336d3c1357db98
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-26 18:25:38 +1300

    Implement entry updating test

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -63,6 +63,17 @@ func TestAdd(t *testing.T) {
 	})
 }
 
+func TestUpdate(t *testing.T) {
+	word := "test"
+	definition := "this is just a test"
+	dictionary := Dictionary{word: definition}
+	newDefinition := "new definition"
+
+	dictionary.Update(word, newDefinition)
+
+	assertDefinition(t, dictionary, word, newDefinition)
+}
+
 
 
 

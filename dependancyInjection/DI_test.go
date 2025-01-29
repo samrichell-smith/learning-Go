a0b3ae1fbcb84741ab7875a75f98688dc63f1bd0
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-29 16:36:12 +1300

    Add Greet unit test

--- dependancyInjection/DI_test.go
+++ dependancyInjection/DI_test.go
@@ -1,1 +1,18 @@
 package DI
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Sam")
+
+	got := buffer.String()
+	want := "Hello, Sam"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}

7b0e4e58af24060a3e1644d53971228c395423bd
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-29 16:54:27 +1300

    Implement basic web server as output

--- dependancyInjection/DI.go
+++ dependancyInjection/DI.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+
+
 func main() {
-	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
 }

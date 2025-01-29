46a4ee66eac5b167f34745371de158fde256d674
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-29 20:56:23 +1300

    Add timeline tracking for countdown sleep operations

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -15,6 +15,8 @@ func main() {
 
 const finalWord = "Go!"
 const countdownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 type Sleeper interface {
 	Sleep()
@@ -25,6 +27,20 @@ type SpySleeper struct {
 }
 
 type DefaultSleeper struct{}
+
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
 
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
@@ -38,7 +54,7 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 
 	for i := countdownStart; i>0; i-- {
 		fmt.Fprintln(out, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 
 	fmt.Fprint(out, finalWord)

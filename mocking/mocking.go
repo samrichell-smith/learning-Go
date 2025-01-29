e46dd5cf74bd95ea664d5bcb494785f9cadd143b
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-29 17:54:57 +1300

    Mock sleep function

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
@@ -14,10 +15,23 @@ func main() {
 const finalWord = "Go!"
 const countdownStart = 3
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep(){
+	s.Calls++
+}
+
 func Countdown(out io.Writer) {
 
 	for i := countdownStart; i>0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Fprint(out, finalWord)

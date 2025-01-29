61c561b43be266807f8a42bd6efb24218f943b64
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-29 21:24:39 +1300

    Implement iterator for countdown

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
@@ -69,10 +70,19 @@ func (s *SpySleeper) Sleep(){
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 
-	for i := countdownStart; i>0; i-- {
+	for i := range countDownFrom(3) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
-
 	fmt.Fprint(out, finalWord)
+}
+
+func countDownFrom(from int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := from; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
 }

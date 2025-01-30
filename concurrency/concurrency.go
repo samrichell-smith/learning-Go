915bd04e88c0c1ee6bf89d9823d4c0613ec90af5
Sam Richell-Smith <samrs6012@gmail.com>
2025-01-30 19:24:24 +1300

    Implement website checking tests

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -2,7 +2,7 @@ package concurrency
 
 type WebsiteChecker func(string) bool
 
-func CheckWebsite(wc WebsiteChecker, urls []string) map[string]bool {
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {

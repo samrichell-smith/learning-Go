123380466a3aad4b1227b7a64b7f08ba422c598e
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-04 18:12:20 +1300

    Update race function for error handling

--- select/select.go
+++ select/select.go
@@ -1,15 +1,19 @@
 package selectpack
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
-func Racer(URL1, URL2 string) (winner string) {
+func Racer(URL1, URL2 string) (winner string, error error) {
 	select {
 	case <-ping(URL1):
-		return URL1
+		return URL1, nil
 	case <-ping(URL2):
-		return URL2
+		return URL2, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", URL1, URL2)
 	}
 }
 

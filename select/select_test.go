51e8d773221632c6a9fbd126a8916eeac4980e14
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-04 18:09:44 +1300

    update tests for expected error handling

--- select/select_test.go
+++ select/select_test.go
@@ -9,25 +9,45 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
+	t.Run("compares speed of servers, returning the url of the fastet one", func(t *testing.T) {
+		slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(1 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		}))
 
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+		fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 
-	slowServer.Close()
-	fastServer.Close()
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(11 * time.Second)
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(12 * time.Second)
+			w.WriteHeader(http.StatusOK)
+		}))
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		_, err := Racer(slowServer.URL, fastServer.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }

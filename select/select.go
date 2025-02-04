411ffa1a73675cf340b0ab31e246332bf8588684
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-04 17:31:52 +1300

    Implement race condition function

--- select/select.go
+++ select/select.go
@@ -1,5 +1,22 @@
 package selectpack
 
+import (
+	"net/http"
+	"time"
+)
+
 func Racer(URL1, URL2 string) (winner string) {
-	return
+	start1 := time.Now()
+	http.Get(URL1)
+	duration1 := time.Since(start1)
+
+	start2 := time.Now()
+	http.Get(URL2)
+	duration2 := time.Since(start2)
+
+	if duration1 < duration2 {
+		return URL1
+	}
+
+	return URL2
 }

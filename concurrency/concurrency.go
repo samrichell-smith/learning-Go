ee92df78bcbdeee4dcd039919b24b3d32b283744
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-15 22:07:17 +1300

    Refactor  result channel

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -9,6 +9,7 @@ type result struct {
 // makes a map of strings, the website names, as keys to a boolean value as to if it is visited
 // creates a channel that takes structs of type result, which we have previously created
 // for each url, create a goroutine with an anonymous function
+// in each goroutine, we send a struct of type result into the channel we have made, with the given props to serve our results
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)

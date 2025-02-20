95aae1b879d39b6e36a9275d924c5de5fae78737
Sam Richell-Smith <samrs6012@gmail.com>
2025-02-20 14:38:49 +1300

    Add cancel methods

--- context/context_test.go
+++ context/context_test.go
@@ -4,14 +4,21 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type SpyStore struct {
 	response string
+	cancelled bool
 }
 
 func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func TestServer(t *testing.T) {
